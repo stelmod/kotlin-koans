123d56d81b553d59c8ca087a8b6337237ba9c07c
Stylianos Modes <stelmod@gmail.com>
2018-08-26 14:10:18 +0200

    Task 37 solution, string and map builders

--- src/v_builders/n37StringAndMapBuilders.kt
+++ src/v_builders/n37StringAndMapBuilders.kt
@@ -1,7 +1,7 @@
 package v_builders
 
 import util.TODO
-import java.util.*
+import kotlin.collections.HashMap
 
 fun buildStringExample(): String {
     fun buildString(build: StringBuilder.() -> Unit): String {
@@ -19,6 +19,12 @@ fun buildStringExample(): String {
     }
 }
 
+fun buildMap(build: MutableMap<Int, String>.() -> Unit): Map<Int, String> {
+    val m = HashMap<Int, String>()
+    m.build()
+    return m
+}
+
 fun todoTask37(): Nothing = TODO(
     """
         Task 37.
@@ -29,11 +35,11 @@ fun todoTask37(): Nothing = TODO(
 )
 
 fun task37(): Map<Int, String> {
-    todoTask37()
-//    return buildMap {
-//        put(0, "0")
-//        for (i in 1..10) {
-//            put(i, "$i")
-//        }
-//    }
+    return buildMap {
+        put(0, "0")
+        for (i in 1..10) {
+            put(i, "$i")
+        }
+    }
 }
+
